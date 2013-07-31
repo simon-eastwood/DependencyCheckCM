126b4986f15fc0559aa0c444a488c8368cb104c0
Jeremy Long <jeremy.long@gmail.com>
2013-07-31 10:21:31 -0400

    releasing updates from private repo

---
+++ dependency-check-core/src/main/java/org/owasp/dependencycheck/analyzer/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <html>
+ * <head>
+ * <title>org.owasp.dependencycheck.analyzer</title>
+ * </head>
+ * <body>
+ * Analyzers are used to inspect the identified dependencies, collect Evidence,
+ * and process the dependencies.
+ * </body>
+ * </html>
+*/
+
+package org.owasp.dependencycheck.analyzer;
